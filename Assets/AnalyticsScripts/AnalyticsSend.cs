231d858e99e9837b02d90148d8ceed8741fe2542
Christian Thomas <christianthomas35@gmail.com>
2022-03-09 11:30:25 -0600

    Added barebones Unity Analytics scripting
    Note: this sends basic events, still need to include public IP address support.

---
+++ Assets/AnalyticsScripts/AnalyticsSend.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Analytics;
+using Unity.Services.Core;
+
+public class AnalyticsSend : MonoBehaviour
+{
+    // Start is called before the first frame update
+async void Start()
+    {
+        await UnityServices.InitializeAsync();
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
