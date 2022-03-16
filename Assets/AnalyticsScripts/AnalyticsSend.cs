d427c8396f7ea334d2772b09bc917cd81df816e7
Christian Thomas <christianthomas35@gmail.com>
2022-03-16 18:16:37 -0500

    Updated AnalyticsSend.cs
    Updated the primary analytics script to support sending POST requests directly to the AWS endpoint. The Unity Analytics script has been removed.

--- Assets/AnalyticsScripts/AnalyticsSend.cs
+++ Assets/AnalyticsScripts/AnalyticsSend.cs
@@ -1,21 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Unity.Services.Analytics;
-using Unity.Services.Core;
+using UnityEngine.Networking;
+using System.Collections;
+import System
 
-public class AnalyticsSend : MonoBehaviour
+public class MyBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-async void Start()
+    void Start()
     {
-        await UnityServices.InitializeAsync();
-         
+        StartCoroutine(Upload());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Upload()
     {
-        
+        WWWForm form = new WWWForm();
+        form.AddField("type", "game_launch");
+        form.AddField("user_id", "AnalyticsSessionInfo.userId");
+        form.AddField("os", "Debug.Log(SystemInfor.operatingSystem");
+        form.AddField("timestamp", "System.DateTime.UTCNow");
+        form.AddField("version", "1.00");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://3gidp6rat9.execute-api.us-west-2.amazonaws.com/game-e2e-demo/events", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
     }
 }
