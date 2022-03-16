1c65403c1a65c5c5d23735c6c7cfc66dd334c010
Christian Thomas <christianthomas35@gmail.com>
2022-03-16 18:20:48 -0500

    Update AnalyticsSend.cs

--- Assets/AnalyticsScripts/AnalyticsSend.cs
+++ Assets/AnalyticsScripts/AnalyticsSend.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
-import System
 
 public class MyBehavior : MonoBehaviour
 {
@@ -16,7 +15,6 @@ public class MyBehavior : MonoBehaviour
         form.AddField("type", "game_launch");
         form.AddField("user_id", "AnalyticsSessionInfo.userId");
         form.AddField("os", "Debug.Log(SystemInfor.operatingSystem");
-        form.AddField("timestamp", "System.DateTime.UTCNow");
         form.AddField("version", "1.00");
 
         using (UnityWebRequest www = UnityWebRequest.Post("https://3gidp6rat9.execute-api.us-west-2.amazonaws.com/game-e2e-demo/events", form))
