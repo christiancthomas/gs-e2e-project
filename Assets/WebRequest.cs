48b998cee358f29e91af8b6ec533d7efab38c3f8
Christian Thomas <christianthomas35@gmail.com>
2022-03-22 16:51:36 -0500

    Update WebRequest.cs
    Simplified WebRequest.cs

--- Assets/WebRequest.cs
+++ Assets/WebRequest.cs
@@ -1,70 +1,28 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-
-public class WebRequest : MonoBehaviour
-{
-    // Start is called before the first frame update
-    private void Start()
-    {
-        StartCoroutine(MakeRequests());
+using System.Collections;
+ 
+public class WebRequest: MonoBehaviour {
+    void Start() {
+        StartCoroutine(Upload());
     }
+ 
+    IEnumerator Upload() {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add( new MultipartFormDataSection("type" = "game_launch"));
 
-    private IEnumerator MakeRequests() {
-        // POST
-        var dataToPost = new PostData(){type = "game_launch", user_id = '1'};
-        var postRequest = CreateRequest("https://3gidp6rat9.execute-api.us-west-2.amazonaws.com/game-e2e-demo/events", RequestType.POST, dataToPost);
-        AttachHeader(postRequest, "Accept","application/json");
-        AttachHeader(postRequest, "Authorization", "a99527741ba1faf5ba7818d6de3d53f4");
-        AttachHeader(postRequest, "Content-Type", "application/json");
-        yield return postRequest.SendWebRequest();
-        var deserializedPostData = JsonUtility.FromJson<PostResult>(postRequest.downloadHandler.text);
-    }
-
-    private UnityWebRequest CreateRequest(string path, RequestType type = RequestType.POST, object data = null) {
-        var request = new UnityWebRequest(path, type.ToString());
-
-        if (data != null) {
-            var bodyRaw = Encoding.UTF8.GetBytes(JsonUtility.ToJson(data));
-            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        UnityWebRequest www = UnityWebRequest.Post("https://3gidp6rat9.execute-api.us-west-2.amazonaws.com/game-e2e-demo/events", formData);
+        UnityWebRequest header = UnityWebRequest.SetRequestHeader("Accept","application/json");
+        UnityWebRequest header = UnityWebRequest.SetRequestHeader("X-Api-Version", "1.1.0");
+        UnityWebRequest header = UnityWebRequest.SetRequestHeader("Authorization", "a99527741ba1faf5ba7818d6de3d53f4");
+        UnityWebRequest header = UnityWebRequest.SetRequestHeader("Content-Type", "application/json");
+        yield return www.Send();
+ 
+        if(www.isError) {
+            Debug.Log(www.error);
         }
-
-        request.downloadHandler = new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-
-        return request;
+        else {
+            Debug.Log("Form upload complete.");
+        }
     }
-
-    private void AttachHeader(UnityWebRequest request,string key,string value)
-    {
-        request.SetRequestHeader(key, value);
-    }
-}
-
-public enum RequestType {
-    GET = 0,
-    POST = 1,
-    PUT = 2
-}
-
-public class Todo {
-    // Ensure no getters / setters
-    // Typecase has to match exactly
-    public int userId;
-    public int id;
-    public string title;
-    public bool completed;
-}
-
-[Serializable]
-public class PostData {
-    public string type;
-    public string user_id;
-}
-
-public class PostResult
-{
-    public string success { get; set; }
 }
