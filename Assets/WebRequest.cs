ac3a6c06862eb910a63c354568eeccb8eba7f60a
Christian Thomas <christianthomas35@gmail.com>
2022-03-24 10:29:08 -0500

    Update WebRequest.cs

--- Assets/WebRequest.cs
+++ Assets/WebRequest.cs
@@ -3,8 +3,10 @@ using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
  
-public class WebRequest: MonoBehaviour {
-    void Start() {
+public class WebRequest: MonoBehaviour 
+{
+    void Start() 
+    {
         StartCoroutine(Upload());
     }
  
@@ -13,6 +15,7 @@ public class WebRequest: MonoBehaviour {
         formData.Add(new MultipartFormDataSection("type", "game_launch"));
 
         UnityWebRequest www = UnityWebRequest.Post("https://3gidp6rat9.execute-api.us-west-2.amazonaws.com/game-e2e-demo/events", formData);
+        
         {
         www.SetRequestHeader("Authorization", "a99527741ba1faf5ba7818d6de3d53f4");
         www.SetRequestHeader("Content-Type", "application/json");
@@ -20,14 +23,19 @@ public class WebRequest: MonoBehaviour {
         www.SetRequestHeader("X-Api-Version", "1.1.0");
         yield return www.SendWebRequest();
  
-        if(www.result == UnityWebRequest.Result.ConnectionError) {
+        if(www.result == UnityWebRequest.Result.ConnectionError) 
+        {
             Debug.Log(www.error);
         }
-        else {
+        else 
+        {
             Debug.Log("Form upload complete.");
             Debug.Log(www.downloadHandler.data);
+            if (result != null)
+            {
+                result(www.downloadHandler.text);
+            }
         }
         }
-
     }
 }
