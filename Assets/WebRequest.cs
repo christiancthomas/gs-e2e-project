111af36abd502cee8aff99cc6d3883feb917c70c
Christian Thomas <christianthomas35@gmail.com>
2022-03-24 14:54:10 -0500

    Update WebRequest.cs
    Redid web request to fix url-encoding error. Rebuilt to support Json data.

--- Assets/WebRequest.cs
+++ Assets/WebRequest.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Networking;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -8,36 +9,42 @@ public class WebRequest: MonoBehaviour
 {
     void Start() 
     {
-        StartCoroutine(Upload());
+        StartCoroutine(Post());
     }
  
-    public IEnumerator Upload() 
+    public IEnumerator Post(string url, string bodyJsonString)
     {
-        // Request Body
-        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-        formData.Add(new MultipartFormDataSection("type", "game_launch"));
+        // Creating a custom class and Json encoding
+        MyClass myObject = new MyClass();
+        myObject.type = "game_launch";
+        myObject.user_id = AnalyticsSessionInfo.userId;
+        bodyJsonString = JsonUtility.ToJson(myObject);
 
-        // Setting web request endpoint and body data
-        UnityWebRequest www = UnityWebRequest.Post("https://3gidp6rat9.execute-api.us-west-2.amazonaws.com/game-e2e-demo/events", formData);
+        url = "https://3gidp6rat9.execute-api.us-west-2.amazonaws.com/game-e2e-demo/events";
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
+        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+
+        // Request headers
+        request.SetRequestHeader("Authorization", "a99527741ba1faf5ba7818d6de3d53f4");
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Accept","application/json");
+        request.SetRequestHeader("X-Api-Version", "1.1.0");
         
-        // Setting request headers and sending request
-        {
-        www.SetRequestHeader("Authorization", "a99527741ba1faf5ba7818d6de3d53f4");
-        www.SetRequestHeader("Content-Type", "application/json");
-        www.SetRequestHeader("Accept","application/json");
-        www.SetRequestHeader("X-Api-Version", "1.1.0");
-        yield return www.SendWebRequest();
- 
+        // Send request
+        yield return request.SendWebRequest();
+
         // Error handling
         if(www.result == UnityWebRequest.Result.ConnectionError) 
         {
-            Debug.Log(www.error);
+            Debug.Log(request.error);
         }
         else 
         {
             Debug.Log("POST request complete.");
             StringBuilder sb = new StringBuilder();
-            foreach (System.Collections.Generic.KeyValuePair<string, string> dict in www.GetResponseHeaders())
+            foreach (System.Collections.Generic.KeyValuePair<string, string> dict in request.GetResponseHeaders())
             {
                 sb.Append(dict.Key).Append(": \t[").Append(dict.Value).Append("]\n");
             }
@@ -46,8 +53,9 @@ public class WebRequest: MonoBehaviour
             Debug.Log(sb.ToString());
 
             // Print Body
-            Debug.Log(www.downloadHandler.text);
-        }
+            Debug.Log(request.downloadHandler.text);
+
+            Debug.Log("Status Code: " + request.responseCode);
         }
     }
 }
