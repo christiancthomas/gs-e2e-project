06c336f1887a1f452b959c2ca31e38ca3248f38b
Christian Thomas <christianthomas35@gmail.com>
2022-03-24 09:46:14 -0500

    Update WebRequest.cs

--- Assets/WebRequest.cs
+++ Assets/WebRequest.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System.Collections.Generic;
  
 public class WebRequest: MonoBehaviour {
     void Start() {
         StartCoroutine(Upload());
     }
  
-    IEnumerator Upload() {
+    public IEnumerator Upload() {
         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
         formData.Add( new MultipartFormDataSection("type" = "game_launch"));
 
         UnityWebRequest www = UnityWebRequest.Post("https://3gidp6rat9.execute-api.us-west-2.amazonaws.com/game-e2e-demo/events", formData);
-        UnityWebRequest header = UnityWebRequest.SetRequestHeader("Accept","application/json");
-        UnityWebRequest header = UnityWebRequest.SetRequestHeader("X-Api-Version", "1.1.0");
-        UnityWebRequest header = UnityWebRequest.SetRequestHeader("Authorization", "a99527741ba1faf5ba7818d6de3d53f4");
-        UnityWebRequest header = UnityWebRequest.SetRequestHeader("Content-Type", "application/json");
+        www.SetRequestHeader("Accept","application/json");
+        www.SetRequestHeader("X-Api-Version", "1.1.0");
+        www.SetRequestHeader("Authorization", "a99527741ba1faf5ba7818d6de3d53f4");
+        www.SetRequestHeader("Content-Type", "application/json");
         yield return www.Send();
  
         if(www.isNetworkError) {
